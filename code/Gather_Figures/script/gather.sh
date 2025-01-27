2c3af1a5c4a06a31608e8de5a5b6c2135bab086e
Ivory Blakley <IvoryEC@gmail.com>
2025-01-27 12:56:36 -0500

    Consolidate figure images to one folder.

---
+++ code/Gather_Figures/script/gather.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Figure 2
+cp ../../*Clincial_cases/output/NCAT_clinical_cases.pdf ../output/.
+cp ../../*Clincial_cases/output/clinical-cases_state-vs-campus_logScale.pdf ../output/.
+
+# Figure 3
+cp ../../*SARS-in-wastewater/output/SARS-CoV-2_RNA-copies-over-time_1.png ../output/.
+cp ../../*Dorms-and-methods/output/dorm-ww-cor-matrix_rho.pdf ../output/.
+cp ../../*Dorms-and-methods/output/dorm-ww-cor-matrix_p.pdf ../output/.
+cp ../../*Dorms-and-methods/output/between-dorm-correlations.pdf ../output/.
+
+# Figure 4
+cp ../../*SARS-in-wastewater/output/scatter_clinical-cases_vs_RNA.copies_perDorm_together_max.png ../output/.
+cp ../../*SARS-in-wastewater/output/campus-wide-cc_vs_max-rna-copies.pdf ../output/.
+
+# Figure 5
+cp ../../*SARS-in-wastewater/output/PMMoV-vs-SARS_by-sample_withLegend.png ../output/.
+cp ../../*campus-wide-cases/output/SARS-to-PMMoV-ratio_vs_campus-clinical-cases.pdf ../output/.
+
+# Figure 6
+cp ../../*PMMoV-in-AS-vs-Grab/output/sampleMethod_Aggie\ V4.pdf ../output/.
+cp ../../*PMMoV-in-AS-vs-Grab/output/sampleMethod_Barbee.pdf ../output/.
+cp ../../*PMMoV-in-AS-vs-Grab/output/sampleMethod_Aggie\ V4\ by\ PMMoV.pdf ../output/.
+cp ../../*PMMoV-in-AS-vs-Grab/output/sampleMethod_Barbee\ by\ PMMoV.pdf ../output/.
